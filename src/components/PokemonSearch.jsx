3cd786a05610463f47192975f99129ccf1f3f60f
peggy8337 <157562918+peggy8337@users.noreply.github.com>
2024-12-04 15:05:54 -0500

    search component
    change the focus ring from red to blue

--- src/components/PokemonSearch.jsx
+++ src/components/PokemonSearch.jsx
@@ -5,7 +5,7 @@ export default function PokemonSearch({ searchTerm, onSearchChange }) {
       placeholder="Search Pokémon..."
       value={searchTerm} // Controlled input value
       onChange={(e) => onSearchChange(e.target.value)} // Updates searchTerm
-      className="w-full p-3 border-2 border-gray-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-red-500"
+      className="w-full p-3 border-2 border-gray-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
   );
 }
